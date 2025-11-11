0dffeba12fef1ef53fdd5cc089d52a570a1d8a22
Klent Villacruel <klentzoldyck@gmail.com>
2025-11-11 16:18:17 +0800

    Chapter 8 of E-Commerce

---
+++ app/Filament/Resources/Users/RelationManagers/OrdersRelationManager.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources\Users\RelationManagers;
+
+use App\Filament\Resources\Orders\OrderResource;
+use App\Models\Order;
+use Filament\Actions\Action;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DissociateAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class OrdersRelationManager extends RelationManager
+{
+    protected static string $relationship = 'orders';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('id')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('id')
+            ->columns([
+                TextColumn::make('id')
+                    ->searchable()
+                    ->label('Order ID'),
+
+                TextColumn::make('grand_total')
+                    ->money('usd'),
+
+                TextColumn::make('status')
+                    ->badge()
+                    ->color(fn (string $state): string => match($state){
+                        'new' => 'info',
+                        'processing' => 'warning',
+                        'shipped' => 'success',
+                        'delivered' => 'success',
+                        'cancelled' => 'danger'
+                    })
+
+                    ->icon(fn (string $state): string => match($state){
+                        'new' => 'heroicon-m-sparkles',
+                        'processing' => 'heroicon-m-arrow-path',
+                        'shipped' => 'heroicon-o-truck',
+                        'delivered' => 'heroicon-o-check-badge',
+                        'cancelled' => 'heroicon-m-x-circle'
+                    })
+                    ->sortable(),
+
+                    TextColumn::make('payment_method')
+                    ->searchable()
+                    ->sortable(),
+
+                    TextColumn::make('payment_status')
+                    ->searchable()
+                    ->badge()
+                    ->sortable(),
+
+                    TextColumn::make('created_at')
+                    ->label('Order Date')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                
+            ])
+            ->recordActions([
+                Action::make('View Order')
+                    ->url(fn (Order $record): string => OrderResource::getUrl('view', ['record' => $record]))
+                    ->color('info')
+                    ->icon('heroicon-o-eye'),
+
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                //
+            ]);
+    }
+}
--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -5,8 +5,10 @@ namespace App\Filament\Resources\Users;
 use App\Filament\Resources\Users\Pages\CreateUser;
 use App\Filament\Resources\Users\Pages\EditUser;
 use App\Filament\Resources\Users\Pages\ListUsers;
+use App\Filament\Resources\Users\RelationManagers\OrdersRelationManager;
 use App\Filament\Resources\Users\Schemas\UserForm;
 use App\Filament\Resources\Users\Tables\UsersTable;
+use App\Models\Order;
 use App\Models\User;
 use BackedEnum;
 use Filament\Actions\DeleteAction;
@@ -80,7 +82,7 @@ class UserResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            OrdersRelationManager::class
         ];
     }
 
