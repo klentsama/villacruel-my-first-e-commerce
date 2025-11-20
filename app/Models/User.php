823666a693ef394b96928b196fdfb65d39552645
Klent Villacruel <klentzoldyck@gmail.com>
2025-11-20 09:29:53 +0800

    Chapter 21 of E-Commerce

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -51,4 +54,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Order::class);
     }
+
+        public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->email == 'klent@gmail.com';
+    }
+
 }
