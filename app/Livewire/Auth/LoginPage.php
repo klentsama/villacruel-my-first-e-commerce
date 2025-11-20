b59a9c4714d0e89665eed987707fd4a8aed74ca6
Klent Villacruel <klentzoldyck@gmail.com>
2025-11-20 08:31:28 +0800

    Chapter 20 of E-Commerce

--- app/Livewire/Auth/LoginPage.php
+++ app/Livewire/Auth/LoginPage.php
@@ -20,7 +20,7 @@ class LoginPage extends Component
             'password' => 'required|min:6|max:255',
         ]);
 
-        if(Auth::attempt(['email' => $this->email, 'password' => $this->password])){
+        if(!Auth::attempt(['email' => $this->email, 'password' => $this->password])){
             session()->flash('error', 'Invalid credentials');
             return;
         }
